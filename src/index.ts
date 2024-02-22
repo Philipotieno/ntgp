2138c797dc3e5a390086f9301d80e4df957ffad7
Philip Otieno <iamphilipotieno@gmail.com>
2024-02-22 08:51:29 +0300

    chore:add graphql schema for testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello Mitch');
+import 'graphql-import-node';
+import { execute, parse } from 'graphql';
+import { schema } from './schema';
+
+async function main() {
+  const myQuery = parse(`query { info }`);
+  const results = await execute({
+    schema,
+    document: myQuery,
+  });
+  console.log(results);
+}
+
+main();
